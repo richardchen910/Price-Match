2150b900e1df85faf48063fa8983e64b886b6b54
Richard Chen <richardchen910@gmail.com>
2018-08-30 22:03:53 -0700

    Can edit and delete items

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_mysqldb import MySQL 
-from wtforms import ValidationError
+from wtforms import Form, StringField, DecimalField, TextAreaField, validators
 from passlib.hash import sha256_crypt
 import MySQLdb
 from functools import wraps
@@ -145,11 +145,11 @@ def logout():
 @app.route('/items')
 @is_logged_in
 def items():
-	#Create a cursor
+	# Create a cursor
 	cur = mysql.connection.cursor()
 
 	# Get list of items
-	result = cur.execute("SELECT * FROM items i WHERE %s = i.user_id", [session['id']])
+	result = cur.execute("SELECT * FROM items WHERE %s = user_id", [session['id']])
 	if result > 0:
 		items = cur.fetchall()
 		return render_template('items.html', items=items)
@@ -157,11 +157,88 @@ def items():
 		msg = 'No items found'
 		return render_template('items.html', msg=msg)
 
-	#Close connection
+	# Close connection
 	cur.close()
 
 	return render_template('items.html')
 
+
+class ItemForm(Form):
+	item = StringField('', [validators.DataRequired()], render_kw={"placeholder": "Item"})
+	price = DecimalField('', [validators.DataRequired()], render_kw={"placeholder": "0.00"})
+	store = StringField('', [validators.DataRequired()], render_kw={"placeholder": "Store"})
+	description = TextAreaField('', [validators.Length(max=50)], render_kw={"placeholder": "Add description (optional)"})
+
+
+# Single item
+@app.route('/items/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_item(id):
+	# Create a cursor
+	cur = mysql.connection.cursor()
+
+	# Get item
+	result = cur.execute("SELECT * FROM items WHERE id = %s", [id])
+	item = cur.fetchone()
+
+	# Get form
+	form = ItemForm(request.form)
+
+	# Set form input values
+	form.item.data = item['item']
+	form.price.data = item['price']
+	form.store.data = item['store']
+	if form.description.data == "-":
+		form.description.data = ""
+	else:
+		form.description.data = item['description']
+
+
+	if request.method == 'POST':
+		item = request.form['item']
+		price = float(request.form['price'])
+		store = request.form['store']
+		description = request.form['description']
+		if description == "":
+			description = "-"
+
+		# Create a cursor
+		cur = mysql.connection.cursor()
+
+		# Get item
+		cur.execute("UPDATE items set item=%s, price=%s, store=%s, description=%s WHERE id = %s", (item, price, store, description, id))
+
+		# Commit to DB
+		mysql.connection.commit()
+
+		# Close connection
+		cur.close()
+
+		flash('Item updated successfully', 'success')
+
+		return redirect(url_for('items'))
+
+	return render_template('edit_item.html', form=form)
+
+
+@app.route('/delete_item/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_item(id):
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Delete item
+	cur.execute("DELETE FROM items WHERE id = %s", [id])
+
+	# Commit to DB
+	mysql.connection.commit()
+
+	# Close connection
+	cur.close()
+
+	flash('Item deleted successfully', 'success')
+
+	return redirect(url_for('items'))
 
 
 # Add item
@@ -193,6 +270,8 @@ def add_item():
 		return redirect(url_for('items'))
 
 	return render_template('add_item.html')
+
+
 
 
 @app.route('/cart')
